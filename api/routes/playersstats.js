2694700fe4be1c65b2d02bf277ba457995a51a64
shizzerr <pawel.piwkowski@o2.pl>
2020-01-09 15:26:06 +0100

    update#working

--- api/routes/playersstats.js
+++ api/routes/playersstats.js
@@ -15,10 +15,10 @@ router.get("/", (req, res, next) => {
 });
 router.post("/", (req, res, next) => {
     const playerstats = new playerStat({
-        _id: new mongoose.Types.ObjectId(),
+        _id: new mongoose.Types.PlayerId(),
         nickname: req.body.nickname,
         tries: req.body.tries,
-        result: req.file.result
+        result: req.body.result
     });
     playerstats.save()
         .then(result => {
@@ -29,16 +29,16 @@ router.post("/", (req, res, next) => {
         })
         .catch(err => req.status(500).json({ error: err }));
 });
-router.get("/:pstatsId", (req, res, next) => {
-    const id = req.params.pstatsId;
+router.get("/:PlayerId", (req, res, next) => {
+    const id = req.params.PlayerId;
     playerStat.findById(id).exec().then(doc => {
         res.status(200).json(doc)
     }).catch(err => req.status(500).json({ error: err }));
 
 })
 
-router.delete("/:pstatsId", (req, res, next) => {
-    const id = req.params.pstatsId;
+router.delete("/:PlayerId", (req, res, next) => {
+    const id = req.params.PlayerId;
     playerStat.remove({ _id: id }).exec()
         .then(result => res.status(200).json({
             message: "Usunięcie statystyk gracza o nr " + id
